adde5fbd3795b02763990f218530bb34415f19ff
mengxiangjia074@gmail.com <mengxiangjia074@gmail.com>
2026-09-25 15:57:47 +0800

    chore: remove defect engineering project

--- defect/detection/romav2/device.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-device = torch.device("cpu")
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-elif torch.backends.mps.is_available():
-    device = torch.device("mps")
-else:
-    device = torch.device("cpu")
